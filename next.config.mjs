98eef2c4f4584a6f10c78d51f89089a2de1988e0
bahari <haribahari71@gmail.com>
2024-12-29 23:31:04 +0700

    fix env.prod & config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['res.cloudinary.com'],
+        domains: ['res.cloudinary.com'], // Domain untuk gambar
     },
+    assetPrefix: 'https://uat-v1.octansidn.com', // Prefix untuk file statis
+    basePath: '', // Jika menggunakan subpath, tambahkan path di sini
+    trailingSlash: true, // Menambahkan trailing slash untuk URL
+    reactStrictMode: true, // Opsional, untuk debugging
 };
 
 export default nextConfig;
